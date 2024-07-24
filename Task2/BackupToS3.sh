16ee6a01925ff4bd351e5d45d39304d76ff8326e
sanket <67377938+sanket7133@users.noreply.github.com>
2024-07-24 17:23:48 +0530

    Update BackupToS3.sh

--- Task2/BackupToS3.sh
+++ Task2/BackupToS3.sh
@@ -1,5 +1,6 @@
 #Write a script to automate the backup of a specified directory to a remote server or a cloud storage solution. 
 #The script should provide a report on the success or failure of the backup operation.
+#
 
 #!/bin/bash
 
